3b1ea1409a2b7009b0baca405ef99c3ed66da8f3
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-02-09 21:45:56 +0100

    Creating hero , interactive , our creations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Loopstudios landing page");
+/* Open Menu & Close */
+const nav = document.querySelector(".nav");
+const iconOpen = document.querySelector(".icon-open");
+const iconClose = document.querySelector(".icon-close");
+
+iconOpen.addEventListener("click", () => {
+  nav.classList.add("open");
+});
+iconClose.addEventListener("click", () => {
+  nav.classList.remove("open");
+});
